d9787547321344a1acd68eb0d6cc07f631f7df09
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-10-09 09:13:40 +0500

    thank you page

--- thankyou.php
+++ thankyou.php
@@ -29,7 +29,7 @@ include 'parameters.php';
     <div class="main">
       <seection class="section">
         <div id="margin_zero" class="content has-text-centered is-size-6">Thanks, you are now </div>
-        <div id="margin_zero" class="content has-text-centered is-size-6">authorized on WiFi</div>
+        <div id="margin_zero" class="content has-text-centered is-size-6">authorized on <?php echo htmlspecialchars($business_name); ?> network</div>
       </seection>
     </div>
 
